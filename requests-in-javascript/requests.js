438944d16912e07adab94aae2a7231267df2ef0a
Stephen <35walke774@gmail.com>
2023-10-26 09:14:06 -0400

    committed  project files

---
+++ requests-in-javascript/requests.js
@@ -0,0 +1,14 @@
+const axios = require("axios");
+
+const url = "http://localhost:5001/constellations";
+axios
+  .get(url)
+  .then((response) => {
+    const result = response.data.filter((constellation) => {
+      return constellation.starsWithPlanets < 10;
+    });
+    console.log(result);
+  })
+  .catch((error) => {
+    console.log(error.message);
+  });
